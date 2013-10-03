52276fc553ef739d33c731aa68f7881a928737b9
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-03 16:53:11 +0900

    Initial commit

---
+++ src/jp/shuri/android/SDCardSender/SDCardSender.java
@@ -0,0 +1,15 @@
+package jp.shuri.android.SDCardSender;
+
+import org.acra.collector.CrashReportData;
+import org.acra.sender.ReportSender;
+import org.acra.sender.ReportSenderException;
+
+public class SDCardSender implements ReportSender {
+
+	@Override
+	public void send(CrashReportData arg0) throws ReportSenderException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
